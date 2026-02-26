07c5da5b6652664eefd4687c449a0b3b652d8f84
Nawin chander <nawinnapzy289@gmail.com>
2026-02-26 21:26:28 +0530

    changes

---
+++ Sharding/Node.js/manual_sharding.js
@@ -0,0 +1,34 @@
+const shards = [
+  { name: "shard1", connection: db1 },
+  { name: "shard2", connection: db2 },
+  { name: "shard3", connection: db3 }
+];
+
+function getShard(userId) {
+  const shardIndex = userId % shards.length;
+  return shards[shardIndex];
+}
+
+async function getUser(userId) {
+  const shard = getShard(userId);
+  return shard.connection.query("SELECT * FROM users WHERE id = ?", [userId]);
+}
+
+
+/// sharding stragties
+
+///ranging
+if (userId < 100000) shard1
+else shard2
+
+//// hash based 
+
+userId % totalShards
+
+
+///// geo based
+
+if (country === "US") use US shard
+else use EU shard
+
+
